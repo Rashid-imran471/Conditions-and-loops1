1daae2a448cd5b59ee24c6b7d565daa204233d61
Rashid-imran471 <134418456+Rashid-imran471@users.noreply.github.com>
2023-12-09 23:37:14 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,20 @@
+const stdmarks=100;
+if(stdmarks>90){
+    console.log("A")
+}
+else if(stdmarks>=70 && stdmarks<=90)
+{
+    console.log("B")
+}
+
+else if(stdmarks>=50 && stdmarks<=70)
+{
+    console.log("C")
+}
+else if(stdmarks>50)
+{
+    console.log("F")
+}
+else {
+    console.log("Better luck next year!!")
+}
---
+++ 5.js
@@ -0,0 +1,12 @@
+//ascending order
+let i=1
+while(i<=25){
+    console.log(i);
+    i++;
+}
+//descending order
+let j=25
+while(j>=1){
+    console.log(j);
+    j--;
+}
